b6ead4f4abbbd6b4d10a8427084b303b558b17f7
aj5050 <austincjanuary@gmail.com>
2024-01-13 15:50:28 -0700

    Added delegates and method for the formula evaluator project/program

--- FormulaEvaluator/FormulaEvaluator.cs
+++ FormulaEvaluator/FormulaEvaluator.cs
@@ -1,6 +1,57 @@
-﻿/// <summary>
+﻿using System.Text.RegularExpressions;
+
+/// <summary>
 /// 
 /// </summary>
 public static class Evaluator{
+    private static Stack<String> operatorStack;
+    private static Stack<int> valueStack;
+    public delegate int Lookup(String variable_name);
 
+    public static int Evaluate(String expression, Lookup variableEvaluator)
+    {
+        string[] substrings = Regex.Split(expression, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
+        foreach (string substring in substrings)
+        {
+            if(int.TryParse(substring,out int result))
+            {
+                if(operatorStack.Count != 0 && valueStack.Count!=0 && (operatorStack.Peek() == "*" || operatorStack.Peek()=="/"))
+                {
+                    if (operatorStack.Peek() == "*")
+                    {
+                        valueStack.Push(valueStack.Pop() * result);
+                    }
+                    else
+                    {
+                        valueStack.Push(valueStack.Pop()/result);
+                    }
+                }
+                else
+                {
+                    valueStack.Push(result);
+                }
+                /// this else if statement will assert that the given string is a variable, as the initial if statement shows that it is not an integer, and all variables require a length of at least 2. 
+            }else if (substring.Length > 1)
+            {
+                if (operatorStack.Count != 0 && valueStack.Count != 0 && (operatorStack.Peek() == "*" || operatorStack.Peek() == "/"))
+                {
+                    if (operatorStack.Peek() == "*")
+                    {
+                        /// the variableEvaluator should throw an exception if the variable cannot be found
+                        valueStack.Push(valueStack.Pop() * variableEvaluator(substring));
+                    }
+                    else
+                    {
+                        valueStack.Push(valueStack.Pop() / variableEvaluator(substring));
+                    }
+                }
+                else
+                {
+                    valueStack.Push(variableEvaluator(substring));
+                }
+            }
+        }
+        // TODO...
+        return 0;
+    }
 }
--- README.md
+++ README.md
@@ -11,8 +11,9 @@ Copyright: CS 3500 and Austin January - This work may not be copied for use in A
 # Spreadsheet functionality overview
 Jan 11th: Currently setting up GitHub, README files, and analyzing the assignment. Added all files up until instruction 6 on the "Assignment One - Formula Evaluator" Google doc. I added the "FormulaEvaluatorTester" project
 with it's FrameWork set to net 8.0, since I couldn't add it as a 6.0 as instructed. 
-Jan 13th: Added additional README file for the FormulaEvaluatorTester program.
+Jan 13th: Added additional README file for the FormulaEvaluatorTester program. Added stacks for operators and values, delegates and methods, and finished the first 2 conditions of the evaluator method (the substring is an integer
+or a stack)
 
 # Time Spent
 Jan 11th: 1 1/2 hours
-Jan 13th: 
+Jan 13th: 1 hour 10 min
