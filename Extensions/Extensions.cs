bb7e0e0c6e2343207c6b85ef63c0522b13797813
Austin <austincjanuary@gmail.com>
2024-01-25 12:17:19 -0700

    updated header

--- Extensions/Extensions.cs
+++ Extensions/Extensions.cs
@@ -1,5 +1,25 @@
-﻿namespace Extensions
+﻿/// <summary>
+/// Author:    Austin January
+/// Partner:   None
+/// Date:      1-23-2024
+/// Course:    CS 3500, University of Utah, School of Computing
+/// Copyright: CS 3500 and Austin January - This work may not 
+///            be copied for use in Academic Coursework.
+///
+/// I, Austin January, certify that I wrote this code from scratch and
+/// did not copy it in part or whole from another source.  All 
+/// references used in the completion of the assignments are cited 
+/// in my README file.
+///
+/// File Contents
+///    This file contains private helper methods for the projects in the "Spreadsheet" solution
+/// 
+/// </summary>
+namespace Extensions
 {
+    /// <summary>
+    /// This class will contain private helper methods for the projects in the "Spreadsheet" solution.
+    /// </summary>
     public class Extensions
     {
 
