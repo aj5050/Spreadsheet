4e95a5e229d7c4c9c513e11de36df514f49bf21e
Austin <austincjanuary@gmail.com>
2024-01-30 13:37:52 -0700

    Added test class for Formula.cs, added header comments and README files for test class

---
+++ FormulaTests/FormulaTests.cs
@@ -0,0 +1,27 @@
+/// <summary>
+/// Author:    Austin January
+/// Partner:   None
+/// Date:      1-30-2024
+/// Course:    CS 3500, University of Utah, School of Computing
+/// Copyright: CS 3500 and Austin January - This work may not 
+///            be copied for use in Academic Coursework.
+///
+/// I, Austin January, certify that I wrote this code from scratch and
+/// did not copy it in part or whole from another source.  All 
+/// references used in the completion of the assignments are cited 
+/// in my README file.
+///
+/// File Contents
+/// 
+/// </summary>
+namespace FormulaTests
+{
+    [TestClass]
+    public class FormulaTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
