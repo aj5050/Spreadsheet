c2b35ef7d485d60eb1fddb5b1a4533694a04c9da
Austin <austincjanuary@gmail.com>
2024-02-06 12:50:01 -0700

    added dictionary and dependency graph

--- Spreadsheet/Spreadsheet.cs
+++ Spreadsheet/Spreadsheet.cs
@@ -19,13 +19,36 @@ using SpreadsheetUtilities;
 
 namespace SS
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class Spreadsheet : AbstractSpreadsheet
     {
+        DependencyGraph DG = new DependencyGraph();
+        Dictionary<string, object> Data = new Dictionary<string, object>();
+        /// <summary>
+        ///   Returns the contents (as opposed to the value) of the named cell.
+        /// </summary>
+        /// 
+        /// <exception cref="InvalidNameException"> 
+        ///   Thrown if the name is null or invalid
+        /// </exception>
+        /// 
+        /// <param name="name">The name of the spreadsheet cell to query</param>
+        /// 
+        /// <returns>
+        ///   The return value should be either a string, a double, or a Formula.
+        ///   See the class header summary 
+        /// </returns>
         public override object GetCellContents(string name)
         {
-            throw new NotImplementedException();
-        }
 
+            return null;
+        }
+        /// <summary>
+        /// Returns an Enumerable that can be used to enumerates 
+        /// the names of all the non-empty cells in the spreadsheet.
+        /// </summary>
         public override IEnumerable<string> GetNamesOfAllNonemptyCells()
         {
             throw new NotImplementedException();
