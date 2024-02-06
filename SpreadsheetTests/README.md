2dc915f47c0f75faa97426a8ae08c39d1b0512f2
aj5050 <austincjanuary@gmail.com>
2024-02-05 20:35:52 -0700

    Started assignment 4, added starting test files and README/header comments

---
+++ SpreadsheetTests/README.md
@@ -0,0 +1,24 @@
+﻿```
+Author:     Austin January
+Partner:    None
+Course:     CS 3500, University of Utah, School of Computing
+GitHub ID:  aj5050
+Repo:       https://github.com/aj5050/Spreadsheet
+Date:       1-25-2024 Time (when submission was completed) 
+Project:    SpreadsheetTests
+Copyright:  CS 3500 and Austin January - This work may not be copied for use in Academic Coursework.
+```
+
+# Comments to Evaluators:
+    Code stands on its own.
+# Assignment Specific Topics
+    N/A
+
+# Consulted Peers:
+    
+
+# References:
+
+    1. Title of page - URL
+    2. Title of page - URL
+    3. Title of page - URL
---
+++ SpreadsheetTests/SpreadsheetTests.cs
@@ -0,0 +1,28 @@
+/// <summary>
+/// Author:    Austin January
+/// Partner:   None
+/// Date:      2-5-2024
+/// Course:    CS 3500, University of Utah, School of Computing
+/// Copyright: CS 3500 and Austin January - This work may not 
+///            be copied for use in Academic Coursework.
+///
+/// I, Austin January, certify that I wrote this code from scratch and
+/// did not copy it in part or whole from another source.  All 
+/// references used in the completion of the assignments are cited 
+/// in my README file.
+///
+/// File Contents
+///     
+/// 
+/// </summary>
+namespace SpreadsheetTests
+{
+    [TestClass]
+    public class SpreadsheetTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
