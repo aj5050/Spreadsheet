21955dde44fc00f16a722ee224007195e66442d4
aj5050 <austincjanuary@gmail.com>
2024-02-05 20:35:20 -0700

    Started Assignment 4, added starter files including README and headder comments

---
+++ Spreadsheet/Spreadsheet.cs
@@ -0,0 +1,54 @@
+﻿/// <summary>
+/// Author:    Austin January
+/// Partner:   None
+/// Date:      2-5-2024
+/// Course:    CS 3500, University of Utah, School of Computing
+/// Copyright: CS 3500 and Austin January - This work may not 
+///            be copied for use in Academic Coursework.
+///
+/// I, Austin January, certify that I wrote this code from scratch and
+/// did not copy it in part or whole from another source.  All 
+/// references used in the completion of the assignments are cited 
+/// in my README file.
+///
+/// File Contents
+///     
+/// 
+/// </summary>
+using SpreadsheetUtilities;
+
+namespace SS
+{
+    public class Spreadsheet : AbstractSpreadsheet
+    {
+        public override object GetCellContents(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<string> GetNamesOfAllNonemptyCells()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ISet<string> SetCellContents(string name, double number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ISet<string> SetCellContents(string name, string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ISet<string> SetCellContents(string name, Formula formula)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IEnumerable<string> GetDirectDependents(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
