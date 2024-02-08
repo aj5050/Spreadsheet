963953e8521384c00498a952c1a48b1dcb9c1212
Austin <austincjanuary@gmail.com>
2024-02-08 13:40:19 -0700

    added 21 tests, all pass with full coverage of Spreadsheet.cs

--- SpreadsheetTests/SpreadsheetTests.cs
+++ SpreadsheetTests/SpreadsheetTests.cs
@@ -15,14 +15,171 @@
 ///     
 /// 
 /// </summary>
-namespace SpreadsheetTests
+using SpreadsheetUtilities;
+namespace SS
 {
     [TestClass]
     public class SpreadsheetTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        Spreadsheet spreadsheet;
+        [TestInitialize]
+        public void testInitialize()
         {
+            spreadsheet = new Spreadsheet();
+        }
+        [TestMethod]
+        [ExpectedException(typeof(InvalidNameException))]
+        public void TestGetInvalidCell()
+        {
+            
+            spreadsheet.GetCellContents("1A");
+        }
+        [TestMethod]
+        public void TestGetValidCells()
+        {
+            spreadsheet.GetCellContents("x");
+            spreadsheet.GetCellContents("X");
+            spreadsheet.GetCellContents("x_");
+            spreadsheet.GetCellContents("_x");
+            spreadsheet.GetCellContents("x_y");
+            spreadsheet.GetCellContents("z_15");
+            spreadsheet.GetCellContents("___");
+            spreadsheet.GetCellContents("_");
+        }
+        [TestMethod]
+        [ExpectedException(typeof(InvalidNameException))]
+        public void TestGetInvalidCellNum()
+        {
+            spreadsheet.GetCellContents("1");
+        }
+        [TestMethod]
+        [ExpectedException(typeof(InvalidNameException))]
+        public void TestGetInvalidCellOperator()
+        {
+            spreadsheet.GetCellContents("+");
+        }
+        [TestMethod]
+        [ExpectedException(typeof(InvalidNameException))]
+        public void TestGetNullCell()
+        {
+            spreadsheet.GetCellContents(null);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(InvalidNameException))]
+        public void TestSetInvalidCell()
+        {
+            spreadsheet.SetCellContents("1A", 0);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(InvalidNameException))]
+        public void TestSetInvalidCellText()
+        {
+            spreadsheet.SetCellContents("1A", "text");
+        }
+        [TestMethod]
+        [ExpectedException(typeof(InvalidNameException))]
+        public void TestSetInvalidCellFormula()
+        {
+            spreadsheet.SetCellContents("1A", new Formula("2+2"));
+        }
+        [TestMethod]
+        [ExpectedException(typeof(CircularException))]
+        public void TestSetCyclicCellFormula()
+        {
+            spreadsheet.SetCellContents("A1", new Formula("A1+2"));
+        }
+        [TestMethod]
+        [ExpectedException(typeof(InvalidNameException))]
+        public void TestSetNullCell()
+        {
+            spreadsheet.SetCellContents(null,0);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(InvalidNameException))]
+        public void TestSetNullCellTextName()
+        {
+            spreadsheet.SetCellContents(null, "text");
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestSetNullCellText()
+        {
+            string input = null;
+            spreadsheet.SetCellContents("A1", input);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(InvalidNameException))]
+        public void TestSetNullCellFormula()
+        {
+            
+            spreadsheet.SetCellContents(null, new Formula("2+2"));
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestSetCellFormulaNull()
+        {
+            Formula f = null;
+            spreadsheet.SetCellContents("A1", f);
+        }
+        [TestMethod]
+        public void TestGetSingleCell()
+        {
+            spreadsheet.SetCellContents("A1", 2.0);
+            Assert.AreEqual(2.0, spreadsheet.GetCellContents("A1"));
+        }
+        [TestMethod]
+        public void TestGetMultipleCells()
+        {
+            spreadsheet.SetCellContents("A1", 2.0);
+            spreadsheet.SetCellContents("B1", new Formula("A1+2"));
+            spreadsheet.SetCellContents("C1", new Formula("B1 + A1"));
+            HashSet<string> expected = new HashSet<string> { "A1", "B1", "C1" };
+            HashSet<string> actual = spreadsheet.GetNamesOfAllNonemptyCells().ToHashSet();
+            Assert.IsTrue(expected.ToString() == actual.ToString());
+        }
+        [TestMethod]
+        public void TestSetSingleCell()
+        {
+            foreach (string name in spreadsheet.SetCellContents("A1",2.0))
+            {
+                Assert.IsTrue(name == "A1");
+            }
+        }
+        [TestMethod]
+        public void TestSetMultipleCell()
+        {
+            spreadsheet.SetCellContents("A1", 2.0);
+            spreadsheet.SetCellContents("B1", new Formula("A1+2"));
+            spreadsheet.SetCellContents("C1", new Formula("B1 + A1"));
+            ISet<string> actual = spreadsheet.SetCellContents("A1", 3.0);
+            HashSet<string> expected = new HashSet<string> { "A1", "B1", "C1" };
+            Assert.IsTrue(expected.ToString()==actual.ToString());
+        }
+        [TestMethod]
+        public void TestSetSingleTextCell()
+        {
+            spreadsheet.SetCellContents("A1", "3");
+            Assert.AreEqual("3", spreadsheet.GetCellContents("A1"));
+        }
+        [TestMethod]
+        public void TestSetMultipleTextCell()
+        {
+            spreadsheet.SetCellContents("A1", "test");
+            spreadsheet.SetCellContents("B1", new Formula("A1+2"));
+            spreadsheet.SetCellContents("C1", new Formula("B1 + A1"));
+            ISet<string> actual = spreadsheet.SetCellContents("A1", "tests");
+            HashSet<string> expected = new HashSet<string> { "A1", "B1", "C1" };
+            Assert.IsTrue(expected.ToString() == actual.ToString());
+        }
+        [TestMethod]
+        public void TestSetMultipleForula()
+        {
+            spreadsheet.SetCellContents("A1", new Formula("2+2"));
+            spreadsheet.SetCellContents("B1", new Formula("A1+2"));
+            spreadsheet.SetCellContents("C1", new Formula("B1 + A1"));
+            ISet<string> actual = spreadsheet.SetCellContents("A1", new Formula("2+3"));
+            HashSet<string> expected = new HashSet<string> { "A1", "B1", "C1" };
+            Assert.IsTrue(expected.ToString() == actual.ToString());
         }
     }
 }
